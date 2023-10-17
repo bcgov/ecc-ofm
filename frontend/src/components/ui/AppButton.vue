3a332ccf15e4b310e742abe19c6fa7cf6b8f5838
weskubo-cgi <80127167+weskubo-cgi@users.noreply.github.com>
2023-10-17 13:30:45 -0700

    Add Test workflow (#17)
    * Added AppButton component.
    
    * Fixed repo name.
    
    * Fixed image name.
    
    * Fixed variable usage.
    
    * Fixed frontend image name.
    
    * Fixed backend image name.
    
    * Updated frontend dc to allow multiple deployment configs (dev, test) in single project.
    
    * Fixed invalid env reference.
    
    * Removed ENVIRONMENT variable.
    
    * Removed autoscaler temporarily.
    
    * Fixed formatting issue.
    
    * Updated backend dc/workflow to support multiple deployments to a single project.
    
    * Updated Route/Service.
    
    * Fixed autoscaler.
    
    * Created test deployment.
    
    * Fixed typo.
    
    * Cleaned up frontend workflow and added zap scan.
    Initial version of backend workflow.
    
    * Fixed namespace.
    Enabled zap scan.
    
    * Fixed namespace reference.
    Cleanup.
    
    * Fixed naming.
    
    * Scaled back replicas.
    
    * Fixed typo.
    
    * Updated replicas.
    
    * Reduced frontend replicaes to lessen utilization.
    
    * Reduced replicas.
    
    ---------
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>

---
+++ frontend/src/components/ui/AppButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-btn class="BC-Gov-PrimaryButton" v-bind="$attrs">
+    <slot />
+  </v-btn>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+}
+</script>
+<style scoped>
+.BC-Gov-PrimaryButton {
+  background-color: #003366;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  margin: 8px; /* ma-2 */
+  padding: 0 12px; /* px-3 */
+  /* padding: 12px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  font-size: 18px;
+  font-family:
+    ‘BCSans’,
+    ‘Noto Sans’,
+    Verdana,
+    Arial,
+    sans-serif;
+  font-weight: 700;
+  letter-spacing: 1px;
+  cursor: pointer; */
+}
+
+.BC-Gov-PrimaryButton:hover {
+  text-decoration: underline;
+  opacity: 0.8;
+}
+
+:focus {
+  outline: 4px solid #3b99fc;
+  outline-offset: 1px;
+}
+
+.BC-Gov-PrimaryButton:active {
+  opacity: 1;
+}
+</style>
