7e5e216b35dc7e85501ca83d8141620bd6085e24
Le <viet.le@cgi.com>
2023-11-04 12:48:31 -0700

    OFMCC-364 - minor UI change

--- frontend/src/components/messages/NewRequestConfirmationDialog.vue
+++ frontend/src/components/messages/NewRequestConfirmationDialog.vue
@@ -3,16 +3,16 @@
     <template #content>
       <v-row>
         <v-col align="center">
-          <p class="pt-4">Your message has been submitted.</p>
-          <p class="pt-4">Reference: {{ referenceNumber }}</p>
-          <p class="pt-4">Typical response times are 3-5 business days.</p>
+          <p class="pt-4 text-h5">Your message has been submitted.</p>
+          <p class="pt-4 text-h5">Reference: {{ referenceNumber }}</p>
+          <p class="pt-4 text-h5">Typical response times are 3-5 business days.</p>
         </v-col>
       </v-row>
     </template>
     <template #button>
       <v-row justify="space-around">
         <AppButton id="return-home-button" :isPrimary="false" size="large" width="200px" @click="returnToHome()">Return to home</AppButton>
-        <AppButton id="view-message-button" size="large" width="200px" @click="viewMessages()">View messages</AppButton>
+        <AppButton id="view-messages-button" size="large" width="200px" @click="viewMessages()">View messages</AppButton>
       </v-row>
     </template>
   </AppDialog>
