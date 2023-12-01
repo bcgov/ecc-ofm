9896f81613415f1298ea06b13cb747f59129394c
jostorey <johnathan.storey@cgi.com>
2023-11-30 17:59:16 -0800

    QA code changes.

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container v-if="usersAndFacilities" class="pa-2 ma-0 w-100">
     <v-row>
-      <v-col class="text-start sub-header pt-0">
-        Manage users
+      <v-col class="pt-0">
+        <h3>Manage users</h3>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="auto">
-        <v-btn @click="toggleShowFilter()">
+        <AppButton @click="toggleShowFilter()" variant="text">
           Filter by facility
           <v-icon right>mdi-filter</v-icon>
-        </v-btn>
+        </AppButton>
       </v-col>
       <v-col cols="3">
         <v-text-field
@@ -19,7 +19,10 @@
           placeholder="Filter by facility name"></v-text-field>
       </v-col>
       <v-col class="d-flex justify-end align-end">
-        <span class="interactive-text-bold">+ Add new user</span>
+        <AppButton variant="text">
+          <v-icon left>mdi-plus</v-icon>
+          Add new user
+        </AppButton>
       </v-col>
     </v-row>
     <v-row>
@@ -37,11 +40,9 @@
 
             <!-- Slot to customize expand row event -->
             <template v-slot:item.data-table-expand="{ item }">
-              <span
-                class="interactive-text"
-                @click.stop="toggleExpand(item)">
+              <AppButton @click.stop="toggleExpand(item)" variant="text">
                 {{ expanded[0] == item.contactId ? 'hide details' : 'view' }}
-              </span>
+              </AppButton>
             </template>
 
             <!-- Slots to translate specific column values into display values -->
@@ -57,51 +58,46 @@
 
             <!-- Slot to customize expand row content -->
             <template v-slot:expanded-row="{ columns, item }">
-              <tr>
-                <td class="no-border-bottom"></td>
-                <td :colspan="7" class="sub-table-header no-border-bottom">
-                  Current facility access
-                </td>
-              </tr>
-              <tr>
-                <td class="no-border-bottom"></td>
-                <td :colspan="5" class="no-border-bottom">
+              <v-row>
+                <v-col cols="1"></v-col>
+                <v-col cols="11">
+                  <h4>Current facility access</h4>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="1"></v-col>
+                <v-col cols="11">
                   <!-- Facilities table -->
                   <v-data-table
                     :headers="headersFacilities"
                     :items="item.facilities"
                     item-key="facilityId"
-                    class="table-facility">
-                    <template #headers="{ columns, isSorted, getSortIcon, toggleSort }">
-              <tr>
-                <th v-for="column in columns" :key="column.key" :style="{ width: column.width }"
-                  class="table-header-facility" @click="toggleSort(column)">
-                  {{ column.title }}
-                  <v-icon v-if="isSorted(column)" size="20" class="pa-0, ma-0">{{ getSortIcon(column) }}</v-icon>
-                </th>
-              </tr>
+                    density="compact">
+                    <template v-slot:item="{ item }">
+                      <v-row>
+                        <v-col>{{ item.name }}</v-col>
+                        <v-col>{{ item.address }}, {{ item.city }}</v-col>
+                      </v-row>
+                    </template>
+                    <template v-slot:bottom><!-- no paging --></template>
+                  </v-data-table>
+                </v-col>
+                <v-col cols="1"></v-col>
+                <v-col cols="11">
+                  <AppButton variant="text">
+                    Deactivate user
+                  </AppButton>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="1"></v-col>
+                <v-col cols="11">
+                  <AppButton variant="text">
+                    Add/remove facilities
+                  </AppButton>
+                </v-col>
+              </v-row>
             </template>
-            <template v-slot:item="{ item }">
-              <tr>
-                <td class="table-item-facility">{{ item.name }}</td>
-                <td class="table-item-facility">{{ item.address }}, {{ item.city }}</td>
-              </tr>
-            </template>
-            <template v-slot:bottom><!-- no paging --></template>
-          </v-data-table>
-          </td>
-          <td class="no-border-bottom"></td>
-          <td class="no-border-bottom interactive-text">
-            Deactivate user
-          </td>
-          </tr>
-          <tr>
-            <td></td>
-            <td :colspan="columns.length">
-              <span class="interactive-text">Add/remove facilities</span>
-            </td>
-          </tr>
-          </template>
           </v-data-table>
         </v-skeleton-loader>
       </v-col>
@@ -110,7 +106,8 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'pinia'
+import { mapState } from 'pinia'
+import AppButton from '@/components/ui/AppButton.vue'
 import { useAuthStore } from '@/stores/auth'
 import rolesMixin from '@/mixins/rolesMixin.js'
 import { CRM_STATE_CODES } from '@/utils/constants'
@@ -118,6 +115,7 @@ import { ApiRoutes } from '@/utils/constants'
 import ApiService from '@/common/apiService'
 
 export default {
+  components: { AppButton },
   mixins: [rolesMixin],
   data() {
     return {
@@ -193,28 +191,31 @@ export default {
 
     // TODO: Hard coding of role names in this method is temporary. Will be replaced with endpoint call to get role names.
     /**
-     * Get the role description for role code(s)
+     * Get a comma separated list of role descriptions from a comma separated list of role codes
      */
     getRoleDescriptions(roles) {
       const roleDescriptions = []
       const rolesArray = this.convertStringToArray(roles)
       rolesArray.forEach(role => {
-        switch (role) {
-          case this.ROLES.ADMIN:
-            roleDescriptions.push('Admin')
-            break
-          case this.ROLES.ACCOUNT_MANAGEMENT:
-            roleDescriptions.push('Account Management')
-            break
-          case this.ROLES.FINANCIAL:
-            roleDescriptions.push('Financial')
-            break
-          case this.ROLES.REPORTING:
-            roleDescriptions.push('Reporting')
-            break
-        }
+        roleDescriptions.push(this.getRoleDescription(role))
       })
       return roleDescriptions.join(', ')
+    },
+
+    /**
+     * Get the role description for a role code
+     */
+    getRoleDescription(role) {
+      switch (role) {
+        case this.ROLES.ADMIN:
+          return 'Admin'
+        case this.ROLES.ACCOUNT_MANAGEMENT:
+          return 'Account Management'
+        case this.ROLES.FINANCIAL:
+          return 'Financial'
+        case this.ROLES.REPORTING:
+          return 'Reporting'
+      }
     },
 
     /**
@@ -233,62 +234,6 @@ export default {
 </script>
 
 <style scoped>
-.sub-header {
-  display: flex;
-  color: #003366;
-  font-weight: 600;
-  font-size: 1.3em;
-}
-
-.table-facility {
-  border: 1px solid #D9D9D9;
-}
-
-.table-header-facility {
-  color: black !important;
-  font-weight: bold !important;
-  background-color: #D9D9D9;
-  font-size: .90em;
-  height: 23px !important;
-}
-
-.table-header-facility:hover {
-  cursor: pointer;
-}
-
-.sub-table-header {
-  color: #545454;
-  font-weight: 600 !important;
-}
-
-td.table-item-facility {
-  height: 23px !important;
-  font-size: .90em !important;
-}
-
-.interactive-text,
-.interactive-text-bold {
-  color: #6699cc;
-  cursor: pointer;
-}
-
-.interactive-text {
-  font-weight: 500;
-}
-
-.interactive-text-bold {
-  font-weight: bold;
-}
-
-.interactive-text:hover,
-.interactive-text-bold:hover {
-  text-decoration: underline;
-}
-
-.no-border-bottom {
-  border-bottom: 0px !important;
-}
-
 >>>div.v-data-table-footer {
   padding-top: 20px !important;
 }
