50edc0560d4c854bbbe253a92c89e0b484d35bc6
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2023-12-07 16:01:52 -0800

    Upgraded to Vuetify 3.4.6.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,23 +3,19 @@ import '@bcgov/bc-sans/css/BCSans.css'
 import '@/assets/css/main.css'
 import '@/assets/css/reset.css'
 
-import App from './App.vue'
-import { VDataTable } from 'vuetify/lib/labs/components.mjs'
-import { VDataTableVirtual } from 'vuetify/lib/labs/components.mjs'
-import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
+import moment from 'moment'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { createMetaManager } from 'vue-meta'
-import { createPinia } from 'pinia'
-import moment from 'moment'
-import router from './router'
+
 import vuetify from '@/plugins/vuetify'
+
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
 const pinia = createPinia()
 
-app.component('VDataTable', VDataTable)
-app.component('VDataTableVirtual', VDataTableVirtual)
-app.component('VSkeletonLoader', VSkeletonLoader)
 app.provide('$moment', moment)
 app.use(router).use(createMetaManager()).use(pinia).use(vuetify).mount('#app')
