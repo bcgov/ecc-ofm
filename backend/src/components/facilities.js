225d2d178f3fc8f1e083e14964e2800d89cf0ebd
jostorey <johnathan.storey@cgi.com>
2024-01-09 13:51:48 -0800

    QA fixes: added phone, cell, and email to facility, and inactive licences will no longer appear in facility account management.

--- backend/src/components/facilities.js
+++ backend/src/components/facilities.js
@@ -6,7 +6,7 @@ const HttpStatus = require('http-status-codes')
 
 async function getFacility(req, res) {
   try {
-    const operation = `accounts(${req.params.accountId})?$select=accountid,accountnumber,name,address1_line1,address1_line2,address1_city,address1_postalcode,address1_stateorprovince,ofm_is_mailing_address_different,address2_line1,address2_line2,address2_city,address2_postalcode,address2_stateorprovince,statecode,statuscode`
+    const operation = `accounts(${req.params.accountId})?$select=accountid,accountnumber,name,telephone1,telephone2,emailaddress1,address1_line1,address1_line2,address1_city,address1_postalcode,address1_stateorprovince,ofm_is_mailing_address_different,address2_line1,address2_line2,address2_city,address2_postalcode,address2_stateorprovince,statecode,statuscode`
     const response = await getOperation(operation)
     return res.status(HttpStatus.OK).json(new MappableObjectForFront(response, FacilityMappings).toJSON())
   } catch (e) {
@@ -28,7 +28,7 @@ async function getFacilityContacts(req, res) {
 
 async function getFacilityLicences(req, res) {
   try {
-    const operation = `ofm_licences?$select=ofm_health_authority,ofm_licence,ofm_licenceid,statuscode&$filter=(_ofm_facility_value eq ${req.params.facilityId})`
+    const operation = `ofm_licences?$select=ofm_health_authority,ofm_licence,ofm_licenceid,statuscode&$filter=(_ofm_facility_value eq ${req.params.facilityId}) and (statuscode eq 1)`
     const response = await getOperation(operation)
     const licences = []
     response?.value?.forEach((item) => licences.push(new MappableObjectForFront(item, LicenceMappings).toJSON()))
