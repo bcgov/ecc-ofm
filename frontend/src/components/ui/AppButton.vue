63a6e8c3d3cffeef20607d54ae1ffe642112c760
Le <viet.le@cgi.com>
2024-01-18 14:28:58 -0800

    ofmcc-1048 - minor UI change for AppButton for mobile view

--- frontend/src/components/ui/AppButton.vue
+++ frontend/src/components/ui/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-btn class="text-none" :class="buttonClass" v-bind="$attrs">
-    <span class="button-text"><slot /></span>
+  <v-btn class="text-none px-2" :class="buttonClass" v-bind="$attrs">
+    <span class="button-text overflow-hidden"><slot /></span>
     <v-icon class="button-icon" v-if="$attrs.icon">{{ $attrs.icon }}</v-icon>
   </v-btn>
 </template>
