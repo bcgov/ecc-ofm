eab4d0d8a56582e135aecec5cc7e03907e2069ff
Andy Hu <andy.hu@cgi.com>
2024-01-26 15:12:33 -0800

    login automation and config file changes

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -2,8 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalModifyObstructiveThirdPartyCode: true,
+    chromeWebSecurity: false,
+    experimentalInteractiveRunEvents: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // on("task", {
+      //   generateOTP() {
+      //     require("cypress-otp");
+      //     return null;
+      //   },
+      // });
     },
   },
 });
---
+++ testing/cypress/e2e/examples/3-login-automation/login.cy.js
@@ -0,0 +1,26 @@
+describe("Azure Active Directory Authentication", () => {
+  beforeEach(() => {
+    // log into Azure Active Directory through our sample SPA using our custom command
+    cy.loginToAAD(Cypress.env("aad_username"), Cypress.env("aad_password"));
+    cy.wait(1000);
+    cy.visit("https://mychildcareservicesdev.crm3.dynamics.com");
+    cy.contains("Dynamics 365").should("exist");
+  });
+
+  it("checks nothing", () => {
+    expect(true).to.be.equal(true);
+  });
+  //   it("verifies the user logged in has the correct name", () => {
+  //     cy.get('#table-body-div td:contains("name") + td').should(
+  //       "contain",
+  //       `${Cypress.env("aad_name")}`
+  //     );
+  //   });
+
+  //   it("verifies the user logged in has the correct preferred name", () => {
+  //     cy.get('#table-body-div td:contains("preferred_username") + td').should(
+  //       "contain",
+  //       `${Cypress.env("aad_username")}`
+  //     );
+  //   });
+});
