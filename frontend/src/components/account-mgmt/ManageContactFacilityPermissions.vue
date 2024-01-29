cb3629a75eac350f0655f133d6b4817a7c0dd524
jostorey <johnathan.storey@cgi.com>
2024-01-28 19:16:36 -0800

    Implementation for OFMCC-1100 - Update Contacts Expense Authority

---
+++ frontend/src/components/account-mgmt/ManageContactFacilityPermissions.vue
@@ -0,0 +1,250 @@
+<template>
+  <v-row>
+    <v-col class="ml-6 pr-9 pt-0 pb-0">
+      <h4>{{ title }}</h4>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12" class="ml-6 pr-9 pt-0">
+      <v-card elevation="0" variant="outlined">
+        <v-expansion-panels variant="accordion" v-model="isPanelOpenAdditionalContacts">
+          <v-expansion-panel elevation="0">
+            <v-expansion-panel-title>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-skeleton-loader :loading="loading" type="table-tbody">
+                <div class="w-100">
+                  <v-row v-if="editMode">
+                    <v-col cols="auto">
+                      <AppLabel>Add {{ title }}:</AppLabel>
+                    </v-col>
+                    <v-col cols="3">
+                      <v-autocomplete
+                        id="additional-contact"
+                        ref="autoComplete"
+                        v-model="contactId"
+                        :items="sortedContactsAvailableForAdd"
+                        :disabled="loading"
+                        item-title="fullName"
+                        item-value="contactId"
+                        label="Select contact to add"
+                        density="compact"
+                        variant="outlined"
+                        @update:modelValue="addContact">
+                      </v-autocomplete>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="pt-2">
+                      <v-data-table
+                        :headers="headers"
+                        :items="sortedContacts"
+                        density="compact">
+                        <template v-slot:item.name="{ item }">
+                          {{ item.firstName + ' ' + item.lastName }}
+                        </template>
+                        <template v-slot:item.role="{ item }">
+                          {{ getRoleNameById(item.role) }}
+                        </template>
+                        <template v-slot:item.actions="{ item }">
+                          <v-row v-if="editMode" justify="end">
+                            <v-icon icon="fa:fa-regular fa-trash-can" @click="deleteContact(item)"></v-icon>
+                          </v-row>
+                        </template>
+                        <template v-slot:[`header.actions`]>
+                          <v-row v-if="!editMode" justify="end">
+                            <v-icon icon="fa:fa-regular fa-edit" @click="toggleEditMode()"></v-icon>
+                          </v-row>
+                        </template>
+                      </v-data-table>
+                    </v-col>
+                  </v-row>
+                  <v-row v-if="editMode">
+                    <v-col cols="12" class="pb-4">
+                      <v-row justify="end">
+                        <AppButton id="cancel" :primary="false" size="large" :loading="loading" class="mr-6" @click="cancelEditContacts()">Cancel</AppButton>
+                        <AppButton id="save" :disabled="isSaveDisabled" :primary="true" size="large" :loading="loading" class="mr-4" @click="saveContactsToUpdate()">Save</AppButton>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-skeleton-loader>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import AppButton from '@/components/ui/AppButton.vue'
+import AppLabel from '@/components/ui/AppLabel.vue'
+import rules from '@/utils/rules'
+import { useAppStore } from '@/stores/app'
+import { mapState } from 'pinia'
+
+export default {
+  name: 'ManageContactFacilityPermissions',
+  components: { AppButton, AppLabel },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    contacts: {
+      type: Array,
+      required: true
+    },
+    contactsForAdd: {
+      type: Array,
+      required: true
+    },
+  },
+  emits: ['save-contact-updates'],
+  data() {
+    return {
+      contactId: null,
+      contactsToDisplay: [],
+      contactsAvailableForAdd: [],
+      updatedContactsToAdd: [],
+      updatedContactsToRemove: [],
+      rules,
+      editMode: false,
+      isPanelOpenAdditionalContacts: 0,
+      panel: null,
+      headers: [
+        { title: 'Name', value: 'name', width: '20%' },
+        { title: 'Role', value: 'role', width: '30%' },
+        { title: '', value: 'actions', width: '50%' }
+      ],
+    }
+  },
+  computed: {
+    ...mapState(useAppStore, ['getRoleNameById']),
+    isSaveDisabled() {
+      return this.updatedContactsToAdd?.length === 0 && this.updatedContactsToRemove?.length === 0
+    },
+    sortedContactsAvailableForAdd() {
+      return this.sortContacts(this.contactsAvailableForAdd)
+    },
+    sortedContacts() {
+      return this.sortContacts(this.contactsToDisplay)
+    },
+  },
+  watch: {
+    contacts: {
+      handler: function (val) {
+        this.contactsToDisplay = val
+      },
+      deep: true,
+    },
+    contactsForAdd: {
+      handler: function (val) {
+        this.contactsAvailableForAdd = val
+      },
+      deep: true,
+    },
+  },
+  created() {
+    this.facilityId = this.$route.params.facilityId
+  },
+  methods: {
+    /**
+     * Toggles edit mode indicator on/off
+     */
+    toggleEditMode() {
+      this.editMode = !this.editMode
+    },
+
+    /**
+     * Filter function for the autocomplete type ahead
+     */
+    typeAheadContactsFilter(item, queryText, itemText) {
+      const text = itemText.toLowerCase();
+      const query = queryText.toLowerCase();
+      return text.indexOf(query) > -1;
+    },
+
+    /**
+     * Remove focus from the autocomplete input
+     * NOTE: This is a workaround for the autocomplete component not triggering update event until the input loses focus.
+     */
+    removeFocus() {
+      const autoComplete = this.$refs.autoComplete;
+      if (autoComplete) {
+        autoComplete.focus();
+        autoComplete.blur();
+      }
+    },
+
+    /**
+     * Remove a contact from inputted list by contactId
+     */
+    removeFromList(contacts, contactId) {
+      const index = contacts.findIndex(obj => obj.contactId === contactId)
+      if (index !== -1) {
+        contacts.splice(index, 1);
+      }
+      return contacts
+    },
+
+    /**
+     * Add a contact to the list of contacts to display and remove from the list of contacts available for add
+     */
+    addContact() {
+      if (!this.contactId) {
+        return
+      }
+      const contactToAdd = this.contactsAvailableForAdd.find(item => item.contactId === this.contactId)
+      this.contactsToDisplay.push(contactToAdd)
+      this.updatedContactsToAdd.push(contactToAdd)
+      this.contactsAvailableForAdd = this.removeFromList(this.contactsAvailableForAdd, this.contactId)
+      this.contactId = null
+      this.removeFocus()
+    },
+
+    /**
+     * Remove a contact from the list of contacts to display and add to the list of contacts available for add
+     */
+    deleteContact(contact) {
+      this.contactsToDisplay = this.removeFromList(this.contactsToDisplay, contact.contactId)
+      this.updatedContactsToRemove.push(contact)
+      this.contactsAvailableForAdd.push(contact)
+    },
+
+    /**
+     * Cancel edit mode and reset the contactId
+     */
+    cancelEditContacts() {
+      this.contactId = null
+      this.editMode = false
+    },
+
+    /**
+     * Send emit to save the updated contacts to add and remove
+     */
+    saveContactsToUpdate() {
+      this.$emit('save-contact-updates', this.updatedContactsToAdd, this.updatedContactsToRemove)
+      this.editMode = false
+    },
+
+    /**
+     * Sort contacts by firstName
+     */
+    sortContacts(contacts) {
+      if (!contacts) {
+        return []
+      }
+      return contacts.sort((a, b) => {
+        return a.firstName.localeCompare(b.firstName)
+      })
+    },
+  },
+}
+</script>
+
--- frontend/src/views/account-mgmt/ManageFacilityView.vue
+++ frontend/src/views/account-mgmt/ManageFacilityView.vue
@@ -7,7 +7,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="ml-6 pt-0 pb-0">
+      <v-col class="ml-6 pt-0">
         <FacilityInfo :loading="loading" :facility="facility" class="mt-0" />
       </v-col>
     </v-row>
@@ -69,46 +69,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" class="licence-card-header">
-        <h4>Expense Authority</h4>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" class="ml-6 pr-9 pt-0">
-        <v-card class="pa-6 w-100" variant="outlined">
-          <v-skeleton-loader :loading="loading" type="table-tbody">
-            <div class="w-100">
-              <!-- NOTE: div was required due to dynamic v-row within v-skeleton-loader, otherwise intended row formatting breaks -->
-              <v-row>
-                <v-col>
-                  <AppLabel>Current expense authority:</AppLabel>
-                </v-col>
-                <v-col>
-                  <v-row no-gutters justify="end">
-                    <v-icon icon="fa:fa-regular fa-plus" class="mr-4"></v-icon>
-                    <v-icon v-if="isEditableExpenseAuthority" icon="fa:fa-regular fa-edit" class="mr-4"></v-icon>
-                  </v-row>
-                </v-col>
-              </v-row>
-              <v-row v-for="(item, index) in this.expenseAuthorities" :key="index">
-                <v-col cols="auto">
-                  <AppLabel>Name:</AppLabel>
-                </v-col>
-                <v-col cols="2">{{ item.firstName }} {{ item.lastName }}</v-col>
-                <v-col cols="auto">
-                  <AppLabel>Role:</AppLabel>
-                </v-col>
-                <v-col cols="2">
-                  {{ getRoleNameById(item.role) }}
-                </v-col>
-              </v-row>
-            </div>
-          </v-skeleton-loader>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="ml-6 pr-9 pt-0 pb-0">
+      <v-col class="ml-6 pr-9 pb-0">
         <h4>Primary Contact</h4>
       </v-col>
     </v-row>
@@ -119,25 +80,44 @@
             <div class="w-100">
               <!-- NOTE: div was required due to dynamic v-row within v-skeleton-loader, otherwise intended row formatting breaks -->
               <v-row>
-                <v-col>
-                  <AppLabel>Current primary contact info:</AppLabel>
-                </v-col>
-                <v-col>
-                  <v-row no-gutters justify="end">
-                    <v-icon v-if="primaryContact" icon="fa:fa-regular fa-edit" class="mr-4"></v-icon>
-                  </v-row>
-                </v-col>
-              </v-row>
-              <v-row>
                 <v-col cols="auto">
                   <AppLabel>Name:</AppLabel>
                 </v-col>
-                <v-col cols="2">{{ primaryContact?.firstName }} {{ primaryContact?.lastName }}</v-col>
-                <v-col cols="auto">
+                <v-col cols="2" class="pb-0">
+                  <v-select v-if="editModePrimaryContact"
+                    id="primary-contact"
+                    v-model="primaryContact"
+                    :items="sortedContacts"
+                    :disabled="loading"
+                    item-title="fullName"
+                    label="Select Primary Contact"
+                    :rules="rules.required"
+                    density="compact"
+                    variant="outlined"
+                    return-object>
+                  </v-select>
+                  <template v-else>
+                    {{ primaryContact?.firstName }} {{ primaryContact?.lastName }}
+                  </template>
+                </v-col>
+                <v-col cols="auto" class="pb-0">
                   <AppLabel>Role:</AppLabel>
                 </v-col>
-                <v-col cols="auto">
+                <v-col cols="2" class="pb-0">
                   {{ getRoleNameById(primaryContact?.role) }}
+                </v-col>
+                <v-col class="pb-0">
+                  <v-row v-if="!editModePrimaryContact" no-gutters justify="end">
+                    <v-icon icon="fa:fa-regular fa-edit" @click="toggleEditPrimaryContact()"></v-icon>
+                  </v-row>
+                </v-col>
+              </v-row>
+              <v-row v-if="editModePrimaryContact">
+                <v-col cols="12">
+                  <v-row justify="end">
+                    <AppButton id="cancel" :primary="false" size="large" :loading="loading" class="mr-6" @click="toggleEditPrimaryContact()">Cancel</AppButton>
+                    <AppButton id="save" :primary="true" size="large" :loading="loading" :disabled="hasPrimaryContactChanged" class="mr-4" @click="savePrimaryContact()">Save</AppButton>
+                  </v-row>
                 </v-col>
               </v-row>
             </div>
@@ -145,19 +125,25 @@
         </v-card>
       </v-col>
     </v-row>
+    <ManageContactFacilityPermissions
+      :loading="loading"
+      title="Expense Authorities"
+      :contacts="expenseAuthorities"
+      :contactsForAdd="expenseAuthoritiesAvailableForAdd"
+      @save-contact-updates="saveExpenseAuthorityUpdates" />
+    <ManageContactFacilityPermissions
+      :loading="loading"
+      title="Additional Contacts"
+      :contacts="additionalContacts"
+      :contactsForAdd="additionalContactsAvailableForAdd"
+      @save-contact-updates="saveAdditionalContactUpdates" />
     <v-row>
       <v-col cols="12" md="6">
         <v-row justify="center" justify-md="start" class="pb-2">
-          <AppButton id="back-to-account-mgmt" :primary="false" size="large" width="400px" :to="{ name: 'account-mgmt' }" :loading="loading">
+          <AppButton id="back-to-manage-organization" :primary="false" size="large" width="515px" :to="{ name: 'manage-organization' }" :loading="loading">
             <v-icon class="pb-1">mdi-arrow-left</v-icon>
-            Back to Account Management
+            Back to Update Organization Information
           </AppButton>
-        </v-row>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-row justify="center" justify-md="end" class="pb-2">
-          <AppButton id="cancel" :primary="false" size="large" :to="{ name: 'manage-organization' }" :loading="loading" class="mr-6">Cancel</AppButton>
-          <AppButton id="save" :disabled="true" :primary="true" size="large" :to="{ name: 'account-mgmt' }" :loading="loading">Save</AppButton>
         </v-row>
       </v-col>
     </v-row>
@@ -168,41 +154,64 @@
 import AppButton from '@/components/ui/AppButton.vue'
 import AppLabel from '@/components/ui/AppLabel.vue'
 import FacilityInfo from '@/components/facilities/FacilityInfo.vue'
+import ManageContactFacilityPermissions from '@/components/account-mgmt/ManageContactFacilityPermissions.vue'
+import ApiService from '@/common/apiService'
+import { ApiRoutes } from '@/utils/constants'
 import FacilityService from '@/services/facilityService'
 import alertMixin from '@/mixins/alertMixin'
+import rules from '@/utils/rules'
 import { useAppStore } from '@/stores/app'
 import { mapState } from 'pinia'
 import { useAuthStore } from '@/stores/auth'
 
 export default {
   name: 'ManageFacilityView',
-  components: { AppButton, AppLabel, FacilityInfo },
+  components: { AppButton, AppLabel, FacilityInfo, ManageContactFacilityPermissions },
   mixins: [alertMixin],
   data() {
     return {
       facilityId: null,
       licences: [],
       contacts: [],
+      primaryContact: undefined,
+      primaryContactLastSaved: undefined,
       facility: undefined,
       loading: false,
+      rules,
+      editModePrimaryContact: false,
     }
   },
   computed: {
     ...mapState(useAuthStore, ['userInfo']),
-    ...mapState(useAppStore, ['getRoleNameById', 'healthAuthorities']),
+    ...mapState(useAppStore, ['getRoleNameById', 'healthAuthorities', 'userRoles']),
     expenseAuthorities() {
       return this.contacts?.filter((contact) => contact.isExpenseAuthority)
     },
-    primaryContact() {
-      return this.contacts?.find((contact) => contact.contactId === this.facility?.primaryContactId)
+    additionalContacts() {
+      return this.contacts?.filter((contact) => contact.isAdditionalContact)
     },
-    isEditableExpenseAuthority() {
-      return this.expenseAuthorities?.length !== 0
+    additionalContactsAvailableForAdd() {
+      return this.contacts?.filter((contact) => !contact.isPrimaryContact && !contact.isAdditionalContact)
+    },
+    expenseAuthoritiesAvailableForAdd() {
+      return this.contacts?.filter((contact) => !contact.isExpenseAuthority)
+    },
+    hasPrimaryContactChanged() {
+      return this.primaryContact === this.primaryContactLastSaved
+    },
+    sortedContacts() {
+      if (!this.contacts) return []
+      const contactsCopy = this.contacts.slice()
+      return contactsCopy.sort((a, b) => {
+        return a.firstName.localeCompare(b.firstName)
+      })
     },
   },
   async created() {
     this.facilityId = this.$route.params.facilityId
     await this.loadData()
+    this.primaryContact = this.contacts?.find((contact) => contact.contactId === this.facility?.primaryContactId)
+    this.primaryContactLastSaved = this.primaryContact
   },
   methods: {
     /**
@@ -223,6 +232,9 @@ export default {
     async getContacts() {
       try {
         this.contacts = await FacilityService.getContacts(this.facilityId)
+        this.contacts?.forEach((contact) => {
+          contact.fullName = `${contact.firstName} ${contact.lastName}`
+        })
       } catch (error) {
         this.setFailureAlert('Failed to get contacts for facilityId = ' + this.facilityId, error)
       }
@@ -239,6 +251,9 @@ export default {
       }
     },
 
+    /**
+     * Get facility by facilityId
+     */
     async getFacility() {
       try {
         this.facility = await FacilityService.getFacility(this.facilityId)
@@ -246,7 +261,96 @@ export default {
         this.setFailureAlert('Failed to get Facility information for facilityId = ' + this.facilityId, error)
       }
     },
-  },
+
+    /**
+     * Toggle edit mode for primary contact
+     */
+    toggleEditPrimaryContact() {
+      this.editModePrimaryContact = !this.editModePrimaryContact
+      this.primaryContact = this.primaryContactLastSaved
+    },
+
+    /**
+     * Format property name for display. i.e. property isExpenseAuthority becomes 'Expense Authority'
+     */
+    formatPropertyForDisplay(str) {
+      // Remove 'is' prefix if it exists
+      if (str.startsWith('is')) {
+        str = str.substring(2)
+      }
+      // Split CamelCase into words and capitalize the first letter of each word
+      return str
+        // Insert a space before all caps and convert to lower case
+        .replace(/([A-Z])/g, ' $1').toLowerCase()
+        // Capitalize the first letter of each word
+        .replace(/^./, str => str.toUpperCase())
+        .replace(/\s./g, str => str.toUpperCase())
+    },
+
+    /** 
+     * Save primary contact updates
+     */
+    async savePrimaryContact() {
+      try {
+        if (this.primaryContact === this.primaryContactLastSaved) {
+          return
+        }
+        this.Loading = true
+        this.facility.primaryContactId = this.primaryContact?.contactId
+        await FacilityService.updateFacilityPrimaryContact(this.facility.facilityId, this.facility.primaryContactId)
+        this.primaryContactLastSaved
+        this.editModePrimaryContact = false
+        this.setSuccessAlert('Primary contact updated successfully')
+      } catch (error) {
+        this.setFailureAlert('Failed to update Primary Contact', error)
+      } finally {
+        this.loading = false
+      }
+    },
+
+    /**
+     * Save contact updates
+     * @param {*} property - property to update (i.e isExpenseAuthority or isAdditionalContact)
+     * @param {*} contactsToAdd - contacts to set incoming property to true
+     * @param {*} contactsToRemove - contacts to set incoming property to false
+     */
+    async saveContactUpdates(property, contactsToAdd, contactsToRemove) {
+      this.loading = true
+      try {
+        contactsToAdd.forEach(obj => obj[property] = true)
+        contactsToRemove?.forEach(obj => obj[property] = false)
+        const contactsToUpdate = (contactsToRemove?.length === 0) ? [...contactsToAdd] : [...contactsToAdd, ...contactsToRemove]
+        for (const contact of contactsToUpdate) {
+          try {
+            await ApiService.apiAxios.patch(ApiRoutes.USER_PERMISSIONS_FACILITIES + '/' + contact.bceidFacilityId, contact)
+            this.getContacts()
+          } catch (error) {
+            this.setFailureAlert(`Failed to update ${property} for contactId = ` + contact.contactId, error)
+            throw error
+          }
+        }
+        this.setSuccessAlert(`${this.formatPropertyForDisplay(property)} updates have been saved successfully`)
+      } catch (error) {
+        this.setFailureAlert(`Failure occurred while updating ${this.formatPropertyForDisplay(property)}`, error)
+      } finally {
+        this.loading = false
+      }
+    },
+
+    /**
+     * Save expense authority contact updates
+     */
+    async saveExpenseAuthorityUpdates(contactsToAdd, contactsToRemove) {
+      await this.saveContactUpdates('isExpenseAuthority', contactsToAdd, contactsToRemove)
+    },
+
+    /**
+     * Save additional contact updates
+     */
+    async saveAdditionalContactUpdates(contactsToAdd, contactsToRemove) {
+      await this.saveContactUpdates('isAdditionalContact', contactsToAdd, contactsToRemove)
+    },
+  }
 }
 </script>
 
