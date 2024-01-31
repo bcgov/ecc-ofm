c9c73b8418aa961d57b6565a96050745e02f191f
weskubo-cgi <80127167+weskubo-cgi@users.noreply.github.com>
2024-01-31 11:48:16 -0800

    Removed pageTitle functionality. (#88)
    Added AppBackButton.
    Misc code cleanup.
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>

--- frontend/src/views/account-mgmt/AccountMgmtView.vue
+++ frontend/src/views/account-mgmt/AccountMgmtView.vue
@@ -17,17 +17,14 @@
         </v-list-item>
       </v-list>
     </v-card>
-    <AppButton id="back-home-button" class="mt-2" :primary="false" size="large" width="220px" :to="{ name: 'home' }">
-      <v-icon class="pb-1">mdi-arrow-left</v-icon>
-      Back to Home
-    </AppButton>
+    <AppBackButton id="back-home-button" size="large" width="220px" :to="{ name: 'home' }">Home</AppBackButton>
   </v-container>
 </template>
 
 <script>
-import AppButton from '@/components/ui/AppButton.vue'
+import AppBackButton from '@/components/ui/AppBackButton.vue'
 
 export default {
-  components: { AppButton },
+  components: { AppBackButton },
 }
 </script>
