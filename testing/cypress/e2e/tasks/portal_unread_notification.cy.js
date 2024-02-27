370d7bb36574bfb718e77f9fead2c665a2f34636
AmmaarKhan1 <ammaar.khan@cgi.com>
2024-02-27 09:34:50 -0800

    ofm-1462 - implement peer review changes

--- testing/cypress/e2e/tasks/portal_unread_notification.cy.js
+++ testing/cypress/e2e/tasks/portal_unread_notification.cy.js
@@ -1,105 +1,135 @@
-describe("OFM Portal", () => {
-  const ADD_USERNAME = Cypress.env("aad_username");
-  const ADD_PASSWORD = Cypress.env("aad_password");
-  const CRM_URL = Cypress.env("crm_url");
-  const CRM_BASE_URL = Cypress.env("crm_base_url");
-  const PORTAL_URL = Cypress.env("portal_url");
+const dayjs = require("dayjs");
+const ADD_USERNAME = Cypress.env("aad_username");
+const ADD_PASSWORD = Cypress.env("aad_password");
+const CRM_URL = Cypress.env("crm_url");
+const CRM_BASE_URL = Cypress.env("crm_base_url");
 
-  beforeEach(() => {
-    // log into CRM
-    cy.visit("www.google.ca");
-    cy.loginToAAD(ADD_USERNAME, ADD_PASSWORD, CRM_URL, CRM_BASE_URL);
-  });
+const PORTAL_USERNAME = Cypress.env("portal_username");
+const PORTAL_PASSWORD = Cypress.env("portal_password");
+const PORTAL_URL = Cypress.env("portal_url");
 
-  it("Log into CRM and create notification", () => {
-    cy.visit(CRM_URL);
-    cy.origin(CRM_BASE_URL, () => {
-      cy.on("uncaught:exception", (e) => {
-        if (e.message.includes(" ")) {
-          return false;
-        }
-      });
+const MESSAGE_TIME = new dayjs().format("YYYY-MMM-DD HH:mm:ss");
+const MESSAGE_DATE = new dayjs().format("YYYY-MMM-DD");
 
-      // click on notifications
-      cy.get("#sitemap-entity-NewSubArea_b568f1b1", { timeout: 20000 })
-        .should("be.visible")
-        .click();
+// log into crm and create notifiction
+before(() => {
+  cy.visit(PORTAL_URL);
+  cy.loginToAAD(ADD_USERNAME, ADD_PASSWORD, CRM_URL, CRM_BASE_URL);
 
-      // click on email button
-      cy.get('button[title="Email"]', { timeout: 10000 })
-        .first()
-        .should("be.visible")
-        .click();
-
-      // input the contact field
-      cy.get('button[title="Search"]', { timeout: 5000 })
-        .eq(1)
-        .click()
-        .wait(1000)
-        .type("ofmqa05");
-      cy.contains("qa05", { timeout: 20000 }).should("be.visible").click();
-
-      // enter the subject
-      cy.get('input[aria-label="Subject"]')
-        .click()
-        .clear()
-        .type("Automation Test - Subject");
-
-      // select communication type
-      cy.get('button[title="Search"]').eq(2).click();
-      cy.contains("Action Required").click();
-
-      // click save
-      cy.get('button[aria-label="Save (CTRL+S)"]').click();
-
-      // change activity status
-      cy.get('button[title="More Header Editable Fields"]').click();
-      cy.get('select[title="Open"]').select("Completed");
-
-      // click save
-      cy.get('button[aria-label="Save (CTRL+S)"]').click();
+  cy.visit(CRM_URL);
+  cy.origin(CRM_BASE_URL, { args: { MESSAGE_TIME } }, ({ MESSAGE_TIME }) => {
+    cy.on("uncaught:exception", (e) => {
+      if (e.message.includes(" ")) {
+        return false;
+      }
     });
+
+    // click on notifications
+    cy.get('ul[aria-label="Communication"]')
+      .find('li[aria-label="Notifications"]')
+      .click();
+
+    // click on email button
+    cy.get('button[aria-label="Email"]', { timeout: 20000 })
+      .should("be.visible")
+      .click();
+
+    // input the contact field
+    cy.get(
+      'span[title="To - Enter the account, contact, lead, queue, or user recipients for the email."]'
+    )
+      .parents('div[role="presentation"]')
+      .eq(2)
+      .find('input[aria-label="To, Multiple Selection Lookup"]', {
+        timeout: 20000,
+      })
+      .click()
+      .clear()
+      .type("ofmqa05")
+      .next("button")
+      .click();
+    cy.get("#tab-section2").scrollTo("top");
+    cy.contains("qa05", { timeout: 20000 }).should("be.visible").click();
+
+    // enter the subject
+    cy.get('input[aria-label="Subject"]')
+      .click()
+      .clear()
+      .type(`Automation Test - ${MESSAGE_TIME}`);
+
+    // select communication type
+    cy.get('span[title="Communication Type"]')
+      .parents('div[role="presentation"]')
+      .eq(2)
+      .find(
+        'button[aria-label="Search records for Communication Type, Lookup field"]',
+        {
+          timeout: 20000,
+        }
+      )
+      .click();
+    cy.contains("Action Required").click();
+
+    // click save
+    cy.get('button[aria-label="Save (CTRL+S)"]').click();
+
+    // change activity status
+    cy.get('button[title="More Header Editable Fields"]').click();
+    cy.get('select[title="Open"]').select("Completed");
+
+    // click save
+    cy.get('button[aria-label="Save (CTRL+S)"]').click();
+  });
+});
+
+describe("OFM Portal", () => {
+  beforeEach(() => {
+    // log into portal
+    cy.loginToPortal(PORTAL_USERNAME, PORTAL_PASSWORD, PORTAL_URL);
   });
 
   it("Checks if latest message on portal is Unread", () => {
-    // log into portal
-    cy.loginToPortal(
-      Cypress.env("portal_username"),
-      Cypress.env("portal_password"),
-      Cypress.env("portal_url")
-    );
+    cy.visit(PORTAL_URL);
 
-    cy.origin(PORTAL_URL, () => {
-      // click on mail icon in top right
-      cy.get("#mail_box_button").click();
-      // check latest notification is unread
-      cy.get("table")
-        .find("tr")
-        .eq(2)
-        .find("td")
-        .eq(1)
-        .should("have.text", "Unread");
-    });
+    // click on mail icon in top right
+    cy.get("#mail_box_button").click();
+
+    // check latest notification's read/unread
+    cy.get("table")
+      .find("tr")
+      .eq(2)
+      .find("td")
+      .eq(1)
+      .should("have.text", "Unread");
   });
 
-  it("Checks if latest message subject on portal is Automation Test - Subject", () => {
-    // log into portal
-    cy.loginToPortal(
-      Cypress.env("portal_username"),
-      Cypress.env("portal_password"),
-      Cypress.env("portal_url")
-    );
+  it("Checks if latest message subject on portal is Automation Test - Subject [Correct date time]", () => {
+    cy.visit(PORTAL_URL);
 
-    cy.origin(PORTAL_URL, () => {
-      // click on mail icon in top right
-      cy.get("#mail_box_button").click();
-      // check latest notification's subject
-      cy.get("table")
-        .find("tr")
-        .eq(2)
-        .find("td")
-        .eq(2)
-        .should("have.text", "Automation Test - Subject");
-    });
+    // click on mail icon in top right
+    cy.get("#mail_box_button").click();
+
+    // check latest notification's subject
+    cy.get("table")
+      .find("tr")
+      .eq(2)
+      .find("td")
+      .eq(2)
+      .should("have.text", `Automation Test - ${MESSAGE_TIME}`);
+  });
+
+  it("Checks if latest message date on portal is the Correct Date", () => {
+    cy.visit(PORTAL_URL);
+
+    // click on mail icon in top right
+    cy.get("#mail_box_button").click();
+
+    // check latest notification's date
+    cy.get("table")
+      .find("tr")
+      .eq(2)
+      .find("td")
+      .eq(3)
+      .should("have.text", `${MESSAGE_DATE}`);
   });
 });
--- testing/cypress/support/e2e.js
+++ testing/cypress/support/e2e.js
@@ -7,56 +7,65 @@ Cypress.Commands.add("loginToAAD", (username, password, crmUrl, crmBaseUrl) => {
 
 // add cypress log into Portal command
 Cypress.Commands.add("loginToPortal", (username, password, portalUrl) => {
-  loginViaPortal(username, password, portalUrl);
+  cy.session(
+    `portal-${username}`,
+    () => {
+      loginViaPortal(username, password, portalUrl);
+    },
+    {
+      validate: () => {
+        cy.visit(portalUrl);
+        cy.contains(username).should("exist");
+      },
+    }
+  );
 });
 
 // CRM log in function
 function loginViaAAD(username, password, crmUrl, crmBaseUrl) {
   // create CRM seesion
-  cy.session([username, password], () => {
-    cy.visit(crmUrl);
+  cy.visit(crmUrl);
 
-    cy.origin(crmBaseUrl, () => {
-      cy.on("uncaught:exception", (e) => {
-        if (e.message.includes(" ")) {
-          return false;
-        }
-      });
+  cy.origin(crmBaseUrl, () => {
+    cy.on("uncaught:exception", (e) => {
+      if (e.message.includes(" ")) {
+        return false;
+      }
     });
-
-    // enter username and submit
-    cy.origin(
-      "login.microsoftonline.com",
-      {
-        args: {
-          username,
-        },
-      },
-      ({ username }) => {
-        cy.get('input[type="email"]').type(username, {
-          log: false,
-        });
-        cy.get('input[type="submit"]').click();
-      }
-    );
-
-    // enter password and submit
-    cy.origin(
-      "login.microsoftonline.com",
-      {
-        args: {
-          password,
-        },
-      },
-      ({ password }) => {
-        cy.get('input[type="password"]').type(password, {
-          log: false,
-        });
-        cy.get('input[type="submit"]').click();
-        cy.get('input[type="button"]').click();
-      }
-    );
   });
+
+  // enter username and submit
+  cy.origin(
+    "login.microsoftonline.com",
+    {
+      args: {
+        username,
+      },
+    },
+    ({ username }) => {
+      cy.get('input[type="email"]').type(username, {
+        log: false,
+      });
+      cy.get('input[type="submit"]').click();
+    }
+  );
+
+  // enter password and submit
+  cy.origin(
+    "login.microsoftonline.com",
+    {
+      args: {
+        password,
+      },
+    },
+    ({ password }) => {
+      cy.get('input[type="password"]').type(password, {
+        log: false,
+      });
+      cy.get('input[type="submit"]').click();
+      cy.get('input[type="button"]').click();
+    }
+  );
 }
 
 // Portal log in function
@@ -65,30 +74,12 @@ function loginViaPortal(username, password, portalUrl) {
   cy.visit(portalUrl);
 
   // click on log in button
-  cy.origin(portalUrl, () => {
-    cy.get('a[id="bceid-login"]').click();
-  });
+  cy.get('a[id="bceid-login"]').click();
 
   // enter username
-  cy.origin(
-    "https://logontest7.gov.bc.ca",
-    { args: { username } },
-    ({ username }) => {
-      cy.get('input[id="user"]', { timeout: 3000 }).type(username);
-    }
-  );
+  cy.get('input[id="user"]', { timeout: 3000 }).type(username);
 
-  // enter password and submit
-  cy.origin(
-    "https://logontest7.gov.bc.ca",
-    {
-      args: {
-        password,
-      },
-    },
-    ({ password }) => {
-      cy.get('input[id="password"]').type(password);
-      cy.get('input[type="submit"]').click();
-    }
-  );
+  // enter password
+  cy.get('input[id="password"]').type(password);
+  cy.get('input[type="submit"]').click();
 }
