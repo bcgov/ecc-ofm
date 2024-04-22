74c596667bb9709dc1397ec6ed5688c831c3a6af
Le <vietdung.le@cgi.com>
2024-04-22 09:22:07 -0700

    initial

--- frontend/src/views/reports/ReportingView.vue
+++ frontend/src/views/reports/ReportingView.vue
@@ -1,8 +1,7 @@
 <template>
-  <OrganizationHeader />
   <v-container fluid v-bind="$attrs">
     <h1 class="mb-6">Reporting</h1>
-
+    <!-- 
     <v-card class="px-4 pt-4 pb-2" variant="outlined">
       <v-row no-gutters class="mb-6">
         <v-col cols="12" md="3" lg="2">
@@ -38,127 +37,149 @@
       </v-row>
       <AppButton id="create-survey" :loading="processing" class="my-4" size="large" width="400px" @click="createSurveyResponse">Submit Monthly Report</AppButton>
     </v-card>
-    <hr class="my-8" />
+    <hr class="my-8" /> -->
     <v-row>
-      <v-col cols="auto" class="pb-0" />
-      <v-col cols="12" xs="12" sm="3" md="2" lg="1" xl="1" class="pb-0">
+      <v-col cols="12" sm="2" lg="1">
         <AppLabel>Facility:</AppLabel>
       </v-col>
-      <v-col cols="12" xs="12" sm="6" md="4" lg="4" xl="4" class="pb-0">
+      <v-col cols="12" sm="8" lg="5" xl="4">
         <v-select v-model="selectedFacility" :items="userInfo.facilities" item-title="facilityName" item-value="facilityId" label="Select facility to report on" density="compact" variant="outlined" />
       </v-col>
     </v-row>
-    <v-row>
-      <v-col cols="auto" class="pb-1 pt-0" />
-      <v-col cols="12" xs="12" sm="3" md="2" lg="1" xl="1" class="pb-1 pt-0">
-        <AppLabel>Report Types:</AppLabel>
-      </v-col>
-      <v-col cols="12" xs="12" sm="6" md="5" lg="5" xl="5" class="pb-1 pt-0">
-        <v-row>
-          <v-col class="pb-0 pt-1">
-            <v-checkbox v-model="isCheckedMonthly" density="compact" class="pl-8">Monthly</v-checkbox>
-          </v-col>
-          <v-col class="pb-0 pt-1">
-            <v-checkbox v-model="isCheckedAnnual" density="compact" class="pl-0">Annual</v-checkbox>
-          </v-col>
-          <v-col class="pb-0 pt-1">
-            <v-checkbox v-model="isCheckedOther" density="compact" class="pl-0">Other</v-checkbox>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row></v-row>
-    <v-row>
-      <v-col cols="auto" class="pb-0 pt-0" />
-      <v-col cols="12" xs="12" sm="3" md="2" lg="1" xl="1" class="pb-0 pt-0">
-        <AppLabel>Date Range:</AppLabel>
-      </v-col>
-      <v-col cols="12" xs="12" sm="3" md="2" lg="2" xl="2" class="pb-0 pt-0">
-        <v-menu ref="menuFromDate" v-model="menuFromDate" :close-on-content-click="false" @update:model-value="(val) => (menuFromDate = val)" transition="scale-transition" offset-y min-width="auto">
-          <template #activator="{ on, attrs }">
-            <v-text-field
-              v-model="formattedFromDate"
-              label="From Date"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="{ ...on }"
-              density="compact"
-              variant="outlined"
-              @click:prepend="menuFromDate = !menuFromDate"></v-text-field>
-          </template>
-          <v-date-picker v-model="fromDate" @input="menuFromDate = false" :locale="locale"></v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col cols="12" xs="12" sm="3" md="2" lg="2" xl="2" class="pb-0 pt-0">
-        <v-menu ref="menuEndDate" v-model="menuEndDate" :close-on-content-click="false" @update:model-value="(val) => (menuEndDate = val)" transition="scale-transition" offset-y min-width="auto">
-          <template #activator="{ on, attrs }">
-            <v-text-field
-              v-model="formattedEndDate"
-              label="To Date"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="{ ...on }"
-              density="compact"
-              variant="outlined"
-              @click:prepend="menuEndDate = !menuEndDate"></v-text-field>
-          </template>
-          <v-date-picker v-model="endDate" @input="menuEndDate = false" :locale="locale"></v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="auto" class="pb-0 pt-0" />
-      <v-col cols="12" xs="12" sm="3" md="2" lg="1" xl="1" class="pb-0 pt-0">
-        <AppLabel>Status:</AppLabel>
-      </v-col>
-      <v-col cols="12" xs="12" sm="4" md="4" lg="4" xl="4" class="pb-0 pt-0">
-        <v-select v-model="selectedStatus" :items="statusTypes" item-title="title" item-value="id" label="Select status to report on" density="compact" variant="outlined" clearable />
-      </v-col>
-      <v-col cols="auto" class="pb-0 pt-0" />
-      <v-col cols="auto" class="pb-0 pt-0">
-        <AppLabel>Include Submitted:</AppLabel>
-      </v-col>
-      <v-col cols="auto" class="pb-0 pt-0 pl-0 d-flex align-top">
-        <v-row>
-          <v-col class="pt-1">
-            <v-checkbox v-model="includeSubmitted" density="compact"></v-checkbox>
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col cols="1" />
-      <v-col>
-        <v-row class="d-flex" justify="start">
-          <AppButton id="reset" :primary="false" size="large" width="100px" class="mr-8" @click="resetFilters()">Reset</AppButton>
-          <AppButton id="run-report" size="large" :disabled="!selectedFacility" width="150px" @click="search()">Search</AppButton>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-divider />
-      </v-col>
-    </v-row>
-    <v-row v-if="isSearched">
-      <v-skeleton-loader :loading="loading" type="table-tbody">
-        <v-col>
-          <v-data-table :headers="headers" :items="displayedFacilities" item-key="reportId" :items-per-page="15" density="compact">
-            <template #[`item.alertType`]="{ item }">
-              <v-icon v-if="item.alertType === 'Due'" color="#c48600" size="34" title="Due now">mdi-alert</v-icon>
-              <v-icon v-else-if="item.alertType === 'Overdue'" color="error" size="34" title="Overdue">mdi-alert-octagon</v-icon>
-              <v-icon v-else-if="item.alertType === 'Completed'" color="success" size="34" title="Completed">mdi-check-circle</v-icon>
-            </template>
-            <template #[`item.actions`]="{ item }">
-              <router-link to="">
-                {{ item.actions }}
-              </router-link>
-            </template>
-          </v-data-table>
-        </v-col>
-      </v-skeleton-loader>
-    </v-row>
-    <v-row>
+    <v-card>
+      <v-tabs v-model="tab" bg-color="#ffffff" density="compact" color="#003366">
+        <v-tab value="history">
+          <v-icon size="large">mdi-history</v-icon>
+          Reporting History
+        </v-tab>
+        <v-tab value="analytics">
+          <v-icon size="large">mdi-finance</v-icon>
+          Reporting Analytics
+        </v-tab>
+      </v-tabs>
+      <v-card-text>
+        <v-window v-model="tab">
+          <v-window-item value="history">
+            <v-card variant="outlined" class="pa-4 mb-4">
+              <v-row>
+                <v-col cols="12" lg="6">
+                  <v-row no-gutters>
+                    <v-col cols="12" md="3" lg="2" class="mt-2">
+                      <AppLabel>Report Types:</AppLabel>
+                    </v-col>
+                    <v-col cols="12" md="9" lg="8">
+                      <v-row no-gutters>
+                        <v-col cols="6" sm="3">
+                          <v-checkbox v-model="isCheckedMonthly" hide-details density="compact" label="Monthly" color="primary"></v-checkbox>
+                        </v-col>
+                        <v-col cols="6" sm="3">
+                          <v-checkbox v-model="isCheckedAnnual" hide-details density="compact" label="Annual" color="primary"></v-checkbox>
+                        </v-col>
+                        <v-col cols="6" sm="3">
+                          <v-checkbox v-model="isCheckedOther" hide-details density="compact" label="Other" color="primary"></v-checkbox>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                  <!-- <v-row>
+                <v-col cols="auto" class="pb-0 pt-0" />
+                <v-col cols="12" xs="12" sm="3" md="2" lg="1" xl="1" class="pb-0 pt-0">
+                  <AppLabel>Date Range:</AppLabel>
+                </v-col>
+                <v-col cols="12" xs="12" sm="3" md="2" lg="2" xl="2" class="pb-0 pt-0">
+                  <v-menu
+                    ref="menuFromDate"
+                    v-model="menuFromDate"
+                    :close-on-content-click="false"
+                    @update:model-value="(val) => (menuFromDate = val)"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto">
+                    <template #activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="formattedFromDate"
+                        label="From Date"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="{ ...on }"
+                        density="compact"
+                        variant="outlined"
+                        @click:prepend="menuFromDate = !menuFromDate"></v-text-field>
+                    </template>
+                    <v-date-picker v-model="fromDate" @input="menuFromDate = false" :locale="locale"></v-date-picker>
+                  </v-menu>
+                </v-col>
+                <v-col cols="12" xs="12" sm="3" md="2" lg="2" xl="2" class="pb-0 pt-0">
+                  <v-menu ref="menuEndDate" v-model="menuEndDate" :close-on-content-click="false" @update:model-value="(val) => (menuEndDate = val)" transition="scale-transition" offset-y min-width="auto">
+                    <template #activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="formattedEndDate"
+                        label="To Date"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="{ ...on }"
+                        density="compact"
+                        variant="outlined"
+                        @click:prepend="menuEndDate = !menuEndDate"></v-text-field>
+                    </template>
+                    <v-date-picker v-model="endDate" @input="menuEndDate = false" :locale="locale"></v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row> -->
+                  <v-row no-gutters class="mt-4">
+                    <v-col cols="12" md="3" lg="2" class="my-2">
+                      <AppLabel>Status:</AppLabel>
+                    </v-col>
+                    <v-col cols="12" md="9" lg="8">
+                      <v-select v-model="selectedStatus" :items="statusTypes" item-title="title" item-value="id" label="Select status to report on" density="compact" variant="outlined" clearable />
+                    </v-col>
+                  </v-row>
+                  <!-- <v-row>
+                <v-col cols="4">
+                  <AppLabel>Include Submitted:</AppLabel>
+                </v-col>
+                <v-col cols="1">
+                  <v-checkbox v-model="includeSubmitted" density="compact"></v-checkbox>
+                </v-col>
+              </v-row>
+               -->
+                </v-col>
+                <v-col cols="12" lg="6">
+                  <AppDateFilter />
+                </v-col>
+              </v-row>
+            </v-card>
+            <!-- <div no-gutters class="d-flex justify-end">
+              <AppButton id="reset" :primary="false" size="large" width="100px" class="mr-8" @click="resetFilters()">Reset</AppButton>
+              <AppButton id="run-report" size="large" :disabled="!selectedFacility" width="150px" @click="search()">Search</AppButton>
+            </div> -->
+            <div align="right">
+              <AppButton id="run-report" size="large" :disabled="!selectedFacility" width="150px" @click="search()">Search</AppButton>
+            </div>
+            <h2 class="mt-8 mb-4">Report Details</h2>
+            <v-skeleton-loader :loading="loading" type="table-tbody">
+              <v-data-table :headers="headers" :items="displayedFacilities" item-key="reportId" :items-per-page="15" density="compact">
+                <template #[`item.alertType`]="{ item }">
+                  <v-icon v-if="item.alertType === 'Due'" color="#c48600" size="34" title="Due now">mdi-alert</v-icon>
+                  <v-icon v-else-if="item.alertType === 'Overdue'" color="error" size="34" title="Overdue">mdi-alert-octagon</v-icon>
+                  <v-icon v-else-if="item.alertType === 'Completed'" color="success" size="34" title="Completed">mdi-check-circle</v-icon>
+                </template>
+                <template #[`item.actions`]="{ item }">
+                  <router-link to="">
+                    {{ item.actions }}
+                  </router-link>
+                </template>
+              </v-data-table>
+            </v-skeleton-loader>
+          </v-window-item>
+          <v-window-item value="analytics">Report Analytics</v-window-item>
+        </v-window>
+      </v-card-text>
+    </v-card>
+
+    <v-row class="my-2">
       <v-col cols="5">
         <AppBackButton id="back-home-button" width="220px" :to="{ name: 'home' }">Home</AppBackButton>
       </v-col>
@@ -168,9 +189,10 @@
 
 <script>
 import { SURVEY_RESPONSE_TYPES } from '@/utils/constants'
-import OrganizationHeader from '@/components/organizations/OrganizationHeader.vue'
 import AppLabel from '@/components/ui/AppLabel.vue'
 import AppButton from '@/components/ui/AppButton.vue'
+import AppDateFilter from '@/components/ui/AppDateFilter.vue'
+
 import AppBackButton from '@/components/ui/AppBackButton.vue'
 import ReportsService from '@/services/reportsService'
 import alertMixin from '@/mixins/alertMixin'
@@ -180,7 +202,7 @@ import { useAppStore } from '@/stores/app'
 
 export default {
   name: 'ReportingView',
-  components: { AppLabel, AppButton, AppBackButton, OrganizationHeader },
+  components: { AppLabel, AppButton, AppBackButton, AppDateFilter },
   mixins: [alertMixin],
   data() {
     return {
@@ -203,6 +225,7 @@ export default {
       menuFromDate: false,
       menuEndDate: false,
       locale: 'en',
+      tab: undefined,
       selectedFiscalYear: undefined,
       selectedReportingMonth: undefined,
       statusTypes: [
@@ -212,11 +235,13 @@ export default {
       ],
       headers: [
         { title: 'Alert', key: 'alertType', width: '2%' },
-        { title: 'Title', key: 'title' },
-        { title: 'Report Type', key: 'reportType' },
         { title: 'Report ID', key: 'reportId' },
+
+        { title: 'Title', key: 'title' },
         { title: 'Status', key: 'status' },
-        { title: 'Last Activity Date', key: 'lastActivityDate' },
+
+        { title: 'Report Type', key: 'reportType' },
+        { title: 'Latest Activity', key: 'lastActivityDate' },
         { title: 'Actions', key: 'actions' },
       ],
     }
