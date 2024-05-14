02ba67259cb3b3c4d0ba56185865d6f8ab45af07
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2024-05-13 19:33:30 -0700

    updates from Wes and Viet

--- frontend/src/services/fundingAgreementService.js
+++ frontend/src/services/fundingAgreementService.js
@@ -37,12 +37,23 @@ export default {
     }
   },
 
-  //TODO: other screens may require a table of all previous funding agreements. Add an endpoint and function here to support that functionality.
   // A facility can have only 1 Active funding agreement
   async getActiveFundingAgreementByFacilityId(facilityId) {
     try {
       if (!facilityId) return
       const response = await ApiService.apiAxios.get(`${ApiRoutes.FUNDING_AGREEMENTS}?facilityId=${facilityId}&stateCode=0`)
+      return response?.data[0]
+    } catch (error) {
+      console.log(`Failed to get the list of active funding agreements by application id - ${error}`)
+      throw error
+    }
+  },
+
+  // A facility can have only 1 Active funding agreement
+  async getActiveFundingAgreementByFacilityIdAndStatus(facilityId, statusCode) {
+    try {
+      if (!facilityId) return
+      const response = await ApiService.apiAxios.get(`${ApiRoutes.FUNDING_AGREEMENTS}?facilityId=${facilityId}&stateCode=0&statusCode=${statusCode}`)
       return response?.data[0]
     } catch (error) {
       console.log(`Failed to get the list of active funding agreements by application id - ${error}`)
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -9,13 +9,13 @@
         sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </v-row>
-    <v-row v-if="isLoading">
+    <v-row v-if="loading">
       <v-col v-for="n in 6" :key="n" cols="12" md="6" lg="4">
-        <v-skeleton-loader :loading="isLoading" type="card"></v-skeleton-loader>
+        <v-skeleton-loader :loading="loading" type="card"></v-skeleton-loader>
       </v-col>
     </v-row>
     <v-row v-else>
-      <v-col cols="12" md="6" lg="4" v-if="hasPermission(PERMISSIONS.SEARCH_VIEW_REPORTS)">
+      <v-col v-if="hasPermission(PERMISSIONS.SEARCH_VIEW_REPORTS)" cols="12" md="6" lg="4">
         <v-card class="home-card" prepend-icon="mdi-file-chart-outline" title="Reporting" @click="$router.push({ name: 'reporting' })">
           <v-card-text>
             Donec iaculis nec quam vel congue. Fusce consequat mattis rhoncus. Sed id ipsum sed purus placerat euismod vel ut erat. Nullam ligula leo, fermentum vel interdum sit amet, tempor at nunc.
@@ -37,7 +37,7 @@
           </v-card-text>
         </v-card>
       </v-col>
-      <v-col cols="12" md="6" lg="4" v-if="hasPermission(PERMISSIONS.VIEW_APPLICATIONS)">
+      <v-col v-if="hasPermission(PERMISSIONS.VIEW_APPLICATIONS)" cols="12" md="6" lg="4">
         <v-card class="home-card" prepend-icon="mdi-file-document-multiple-outline" title="Applications" @click="$router.push({ name: 'applications-history' })">
           <v-card-text>
             Etiam nisi erat, dictum finibus arcu feugiat, dictum vestibulum augue. In et auctor urna. Suspendisse potenti. Duis aliquet non ipsum a feugiat. Mauris felis mi, feugiat eu placerat non,
@@ -63,89 +63,28 @@
     </v-row>
   </v-container>
 
-  <AppDialog v-model="isDisplayed" title="Signature Required!" persistent max-width="60%" @close="closeDialog">
-    <template #content>
-      <v-card v-for="fundingAgreement in fundingAgreements" :key="fundingAgreement.facilityId" class="confirm-dialog-text d-flex flex-column mx-md-14 py-6 my-2">
-        <v-row class="mx-md-10">
-          <v-col cols="6" class="text-center">{{ fundingAgreement.fundingAgreementNumber }}</v-col>
-          <v-col cols="6" class="text-center">{{ fundingAgreement.facilityName }}</v-col>
-        </v-row>
-      </v-card>
-      <br />
-      <h2 class="confirm-dialog-text d-flex flex-column text-center">You have one or more Funding Agreements ready to sign.</h2>
-      <div class="confirm-dialog-text d-flex flex-column text-center">Please visit the notification center for more details.</div>
-      <div class="confirm-dialog-text d-flex flex-column text-center">Would you like to take action now?</div>
-      <br />
-    </template>
-    <template #button>
-      <v-row justify="space-around">
-        <v-col cols="12" md="6" class="d-flex justify-center">
-          <AppButton id="dialog-go-back" :primary="false" size="large" width="200px" :loading="isLoading" @click="closeDialog">Not Now</AppButton>
-        </v-col>
-        <v-col cols="12" md="6" class="d-flex justify-center">
-          <AppButton id="dialog-cancel-application" size="large" min-width="250px" max-width="500px" :loading="isLoading" @click="goToFunding">Yes, take me to Funding</AppButton>
-        </v-col>
-      </v-row>
-    </template>
-  </AppDialog>
+  <SignFundingPopup @loading="setloading" />
 </template>
 
 <script>
-import { mapState } from 'pinia'
-import { useAuthStore } from '@/stores/auth'
-import { FUNDING_AGREEMENT_STATUS_CODES } from '@/utils/constants'
-import OrganizationHeader from '@/components/organizations/OrganizationHeader.vue'
-import AppButton from '@/components/ui/AppButton.vue'
 import AppHeroImage from '@/components/ui/AppHeroImage.vue'
-import AppDialog from '@/components/ui/AppDialog.vue'
-import FundingAgreementService from '@/services/fundingAgreementService'
+import OrganizationHeader from '@/components/organizations/OrganizationHeader.vue'
+import SignFundingPopup from '@/components/funding/SignFundingPopup.vue'
 import permissionsMixin from '@/mixins/permissionsMixin.js'
 
 export default {
   name: 'HomeView',
-  components: { AppHeroImage, AppButton, AppDialog, OrganizationHeader },
+  components: { AppHeroImage, OrganizationHeader, SignFundingPopup },
   mixins: [permissionsMixin],
+  emits: ['setLoading'],
   data() {
     return {
-      isLoading: false,
-      isDisplayed: false,
-      fundingAgreements: [],
+      loading: false,
     }
   },
-  computed: {
-    ...mapState(useAuthStore, ['userInfo']),
-  },
-  async created() {
-    this.MAX_FACILITIES_DISPLAYED = 3
-    await this.loadFundingAgreements()
-  },
   methods: {
-    closeDialog() {
-      this.isDisplayed = false
-    },
-    goToFunding() {
-      this.$router.push({ name: 'funding-overview' })
-    },
-    async loadFundingAgreements() {
-      this.isLoading = true
-
-      //I use for of here so I can break out of the loop when I hit 3 facilities.
-      //We show 3 max so the popup doesn't take over the screen, and we save on calls to Dynamics.
-      for (const fac of this.userInfo.facilities) {
-        const fa = await FundingAgreementService.getActiveFundingAgreementByFacilityId(fac.facilityId)
-
-        if (fa?.statusCode === FUNDING_AGREEMENT_STATUS_CODES.SIGNATURE_PENDING) {
-          this.fundingAgreements.push(fa)
-        }
-        if (this.fundingAgreements.length === this.MAX_FACILITIES_DISPLAYED) {
-          break
-        }
-      }
-
-      if (this.fundingAgreements.length > 0) {
-        this.isDisplayed = true
-      }
-      this.isLoading = false
+    setloading(value) {
+      this.loading = value
     },
   },
 }
