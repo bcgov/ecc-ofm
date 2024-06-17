75fb4a95271c442a680d64a2e25aa700894899f5
Le <vietdung.le@cgi.com>
2024-06-17 13:34:04 -0700

    ofmcc-4143 - code cleanup

--- frontend/src/components/reports/PendingReportsTab.vue
+++ frontend/src/components/reports/PendingReportsTab.vue
@@ -28,8 +28,7 @@
         <template #[`item.actions`]="{ item }">
           <v-row no-gutters class="my-2 align-center justify-end justify-md-start">
             <AppButton :primary="false" btn-size="small" class="mr-2" @click="openSurveyResponse(item)">
-              <template v-if="isActiveReportResponse(item)">Update</template>
-              <template v-else>View</template>
+              {{ isActiveReportResponse(item) ? 'Update' : 'View' }}
             </AppButton>
             <AppButton v-if="!isActiveReportResponse(item)" :primary="false" btn-size="small" :disabled="hasInProgressAssistanceRequest(item)" @click="toggleAssistanceRequestDialog(item)">
               Unlock
