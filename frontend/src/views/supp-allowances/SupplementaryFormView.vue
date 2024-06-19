06132fcc7b5851a7e209b0b7ff30e41bcbe2a3f5
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2024-06-19 15:41:00 -0700

    date issue

--- frontend/src/views/supp-allowances/SupplementaryFormView.vue
+++ frontend/src/views/supp-allowances/SupplementaryFormView.vue
@@ -495,8 +495,9 @@ export default {
     setSuppTermDates() {
       const today = new Date()
       const formattedEndDate = new Date(this.fundingAgreement.endDate)
-      const termTwoEndDate = new Date(formattedEndDate.setFullYear(formattedEndDate.getFullYear() - 1))
-      const termOneEndDate = new Date(termTwoEndDate.setFullYear(termTwoEndDate.getFullYear() - 1))
+
+      const termTwoEndDate = new Date(new Date(this.fundingAgreement.endDate).setFullYear(new Date(this.fundingAgreement.endDate).getFullYear() - 1))
+      const termOneEndDate = new Date(new Date(termTwoEndDate).setFullYear(new Date(termTwoEndDate).getFullYear() - 1))
 
       switch (true) {
         //not having a funding agreement or FA end date will only happen if a user navigates to SuppApp right after
