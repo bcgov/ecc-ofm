66e0a3c137cef70f20eb1e87ac3027e7b85a63d0
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2024-06-25 16:08:15 -0700

    Fixed help screen.

--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -1,19 +1,15 @@
 <template>
-  <OrganizationHeader />
   <v-container fluid v-bind="$attrs">
-    <h1>Resources</h1>
+    <h1>Help Centre</h1>
     <AppBackButton id="back-home-button" width="220px" :to="{ name: 'home' }">Home</AppBackButton>
   </v-container>
 </template>
 
 <script>
-import OrganizationHeader from '@/components/organizations/OrganizationHeader.vue'
 import AppBackButton from '@/components/ui/AppBackButton.vue'
 
 export default {
   name: 'HelpView',
-  components: { AppBackButton, OrganizationHeader },
+  components: { AppBackButton },
 }
 </script>
-
-<style scoped></style>
