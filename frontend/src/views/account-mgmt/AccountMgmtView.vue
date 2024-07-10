1f624643532b3f75b6c4fd86f548f1a0ff19f104
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2024-07-10 14:57:11 -0700

    Updated layout.
    (cherry picked from commit ee3b511e772648f3d4fd8c3afe2c093003122a3e)

--- frontend/src/views/account-mgmt/AccountMgmtView.vue
+++ frontend/src/views/account-mgmt/AccountMgmtView.vue
@@ -1,22 +1,34 @@
 <template>
   <v-container fluid>
     <h1>Account Management</h1>
-    <v-card min-width="350px" max-width="50%">
-      <v-list>
-        <v-list-item>
-          <template v-slot:prepend>
-            <v-icon>mdi-office-building</v-icon>
-          </template>
-          <router-link :to="{ name: 'manage-organization' }">Manage Organization/Facilities</router-link>
-        </v-list-item>
-        <v-list-item v-if="hasPermission(PERMISSIONS.MANAGE_USERS_VIEW)">
-          <template v-slot:prepend>
-            <v-icon>mdi-account-group</v-icon>
-          </template>
-          <router-link :to="{ name: 'manage-users' }">Manage Users</router-link>
-        </v-list-item>
-      </v-list>
-    </v-card>
+    <v-row>
+      <v-col v-if="hasPermission(PERMISSIONS.VIEW_ORG_FACILITY)" cols="12" md="6" lg="4">
+        <v-card class="account-card" id="org-facilities-card" prepend-icon="mdi-office-building" title="Manage Organizations/Facilities" @click="$router.push({ name: 'manage-organization' })">
+          <v-card-text>
+            <p>View and update organization/facility details</p>
+            <ul>
+              <li>Mailing address</li>
+              <li>Contact info</li>
+              <li>Primary contact</li>
+              <li>Expense Authority</li>
+              <li>Facility phone/email</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col v-if="hasPermission(PERMISSIONS.MANAGE_USERS_VIEW)" cols="12" md="6" lg="4">
+        <v-card class="account-card" id="manage-users-card" prepend-icon="mdi-account-group" title="Manage Facility Users" @click="$router.push({ name: 'manage-users' })">
+          <v-card-text>
+            <p>Manage facility users</p>
+            <ul>
+              <li>Add new users</li>
+              <li>Edit user role</li>
+              <li>View user contact information</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
     <AppBackButton id="back-home-button" size="large" width="220px" :to="{ name: 'home' }">Home</AppBackButton>
   </v-container>
 </template>
@@ -30,3 +42,19 @@ export default {
   mixins: [permissionsMixin],
 }
 </script>
+
+<style scoped>
+.account-card {
+  border-top: 5px solid #003366 !important;
+  min-height: 250px;
+}
+
+ul {
+  padding-left: 25px;
+  list-style-type: disc;
+}
+
+li {
+  margin-top: 3px;
+}
+</style>
