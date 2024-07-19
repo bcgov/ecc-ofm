668501f7be9614d9be245d100b3c1cf8fb22af10
Le <vietdung.le@cgi.com>
2024-07-19 09:43:04 -0700

    ofmcc-5160 - add due-overdue monthly report banner

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,15 @@
 <template>
+  <v-row v-if="showReportsAlertBanner" no-gutters class="blue-background justify-center pa-2">
+    <v-banner bg-color="#f9f1c6" :lines="$vuetify.display.smAndUp ? 'one' : 'two'" max-width="700px" rounded class="alert-banner">
+      <template #text>
+        <v-icon color="warning" class="mr-2">mdi-alert</v-icon>
+        <strong>You have One or more monthly reports that are due or overdue!</strong>
+      </template>
+      <template #actions>
+        <router-link :to="{ name: 'reporting' }"><strong>Take Action</strong></router-link>
+      </template>
+    </v-banner>
+  </v-row>
   <AppHeroImage />
   <OrganizationHeader />
   <v-container v-bind="$attrs">
@@ -7,9 +18,9 @@
         <p class="home-overview page-overview">Welcome to your Management Portal - Connect with the program, view and manage accounts, applications, and reports</p>
       </v-col>
     </v-row>
-    <v-row v-if="loading">
+    <v-row v-if="isLoading">
       <v-col v-for="n in 6" :key="n" cols="12" md="6" lg="4">
-        <v-skeleton-loader :loading="loading" type="card" />
+        <v-skeleton-loader :loading="isLoading" type="card" />
       </v-col>
     </v-row>
     <v-row v-else>
@@ -46,34 +57,72 @@
     </v-row>
   </v-container>
 
-  <SignFundingPopup v-if="hasPermission(PERMISSIONS.VIEW_FUNDING_AGREEMENT)" @loading="setLoading" />
+  <SignFundingPopup v-if="hasPermission(PERMISSIONS.VIEW_FUNDING_AGREEMENT)" @loading="setLoadingFA" />
   <NewRequestDialog v-if="hasPermission(PERMISSIONS.MANAGE_NOTIFICATIONS)" :show="showAssistanceRequestDialog" @close="toggleAssistanceRequestDialog" />
 </template>
 
 <script>
+import { isEmpty } from 'lodash'
+import { mapState } from 'pinia'
+
 import SignFundingPopup from '@/components/funding/SignFundingPopup.vue'
 import NewRequestDialog from '@/components/messages/NewRequestDialog.vue'
 import OrganizationHeader from '@/components/organizations/OrganizationHeader.vue'
 import AppHeroImage from '@/components/ui/AppHeroImage.vue'
+import alertMixin from '@/mixins/alertMixin'
 import permissionsMixin from '@/mixins/permissionsMixin.js'
+import ReportsService from '@/services/reportsService'
+import { useAuthStore } from '@/stores/auth'
 
 export default {
   name: 'HomeView',
   components: { AppHeroImage, NewRequestDialog, OrganizationHeader, SignFundingPopup },
-  mixins: [permissionsMixin],
-  emits: ['setLoading'],
+  mixins: [alertMixin, permissionsMixin],
   data() {
     return {
       loading: false,
+      loadingFA: false,
       showAssistanceRequestDialog: false,
+      pendingReports: [],
     }
   },
+  computed: {
+    ...mapState(useAuthStore, ['userInfo']),
+    isLoading() {
+      return this.loading || this.loadingFA
+    },
+    showReportsAlertBanner() {
+      return this.hasPermission(this.PERMISSIONS.SUBMIT_DRAFT_REPORTS) && !isEmpty(this.pendingReports)
+    },
+  },
+  async created() {
+    await this.loadPendingReports()
+  },
   methods: {
-    setLoading(value) {
-      this.loading = value
+    setLoadingFA(value) {
+      this.loadingFA = value
     },
     toggleAssistanceRequestDialog() {
       this.showAssistanceRequestDialog = !this.showAssistanceRequestDialog
+    },
+    async loadPendingReports() {
+      try {
+        if (!this.hasPermission(this.PERMISSIONS.SUBMIT_DRAFT_REPORTS)) return
+        this.loading = true
+        this.pendingReports = []
+        await Promise.all(
+          this.userInfo?.facilities?.map(async (facility) => {
+            const response = await ReportsService.getDraftSurveyResponsesByFacility(facility.facilityId)
+            if (!isEmpty(response)) {
+              this.pendingReports = this.pendingReports?.concat(response)
+            }
+          }),
+        )
+      } catch (error) {
+        this.setFailureAlert('Failed to get pending reports for facilities ', error)
+      } finally {
+        this.loading = false
+      }
     },
   },
 }
@@ -82,5 +131,8 @@ export default {
 <style scoped>
 .home-overview {
   text-align: center;
+}
+.alert-banner {
+  border: 1px solid rgb(252, 186, 25);
 }
 </style>
