71c9d3636fe75a657867bcc42e86eeae90a97eec
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2024-07-30 13:12:22 -0700

    Added ignores.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,17 @@
-import js from '@eslint/js';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import pluginVue from 'eslint-plugin-vue';
+import js from '@eslint/js'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import pluginVue from 'eslint-plugin-vue'
 
 export default [
   ...pluginVue.configs['flat/recommended'],
   js.configs.recommended,
   eslintPluginPrettier,
   {
-  rules: {
-    semi: 'off',
-  }
-  }
+    ignores: ['dist/', 'test/'],
+  },
+  {
+    rules: {
+      semi: 'off',
+    },
+  },
 ]
