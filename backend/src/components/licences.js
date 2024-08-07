926074d82d0443311219b6779a517c43389d0487
Le <vietdung.le@cgi.com>
2024-08-07 09:49:45 -0700

    ofmcc-5446 - fix blank operation hour error

--- backend/src/components/licences.js
+++ backend/src/components/licences.js
@@ -1,19 +1,26 @@
 'use strict'
-const { getOperation, patchOperationWithObjectId } = require('./utils')
+const { getOperation, patchOperationWithObjectId, handleError } = require('./utils')
 const { MappableObjectForFront, MappableObjectForBack } = require('../util/mapping/MappableObject')
 const { LicenceDetailsMappings } = require('../util/mapping/Mappings')
 const HttpStatus = require('http-status-codes')
-const log = require('./logger')
 
 async function getLicenceDetails(req, res) {
   try {
-    const operation = `ofm_licence_details?$select=ofm_licence_detailid,ofm_licence_type,ofm_licence_spaces,ofm_operational_spaces,ofm_enrolled_spaces,ofm_operation_from_time,ofm_operations_to_time,ofm_week_days,ofm_weeks_in_operation,ofm_care_type,ofm_overnight_care,ofm_apply_room_split_condition,ofm_room_split_details,statuscode,statecode&$filter=(_ofm_licence_value eq ${req.params.licenceId}) and (statecode eq 0)`
+    const operation = `ofm_licence_details?$select=ofm_licence_detailid,ofm_licence_type,ofm_licence_spaces,ofm_operational_spaces,ofm_enrolled_spaces,ofm_operation_from_time,ofm_operations_to_time,ofm_operation_hours_from,ofm_operation_hours_to,ofm_week_days,ofm_weeks_in_operation,ofm_care_type,ofm_overnight_care,ofm_apply_room_split_condition,ofm_room_split_details,statuscode,statecode&$filter=(_ofm_licence_value eq ${req.params.licenceId}) and (statecode eq 0)`
     const response = await getOperation(operation)
     const licenceDetails = []
-    response?.value?.forEach((item) => licenceDetails.push(new MappableObjectForFront(item, LicenceDetailsMappings).toJSON()))
+    response?.value?.forEach((item) => {
+      const mappedLicenceDetails = new MappableObjectForFront(item, LicenceDetailsMappings).toJSON()
+      // XXX (OFMCC-5436) - In CRM, ofm_operation_from_time (ofm_operations_to_time) gets the hours from ofm_operation_hours_from (ofm_operation_hours_to).
+      // If ofm_operation_hours_from (ofm_operation_hours_to) is null, CRM set ofm_operation_from_time (ofm_operations_to_time) to 12:00 AM as default.
+      // Therefore, we need this logic to overwrite that default value.
+      mappedLicenceDetails.operationFromTime = mappedLicenceDetails.updatableOperationFromTime ? mappedLicenceDetails.operationFromTime : null
+      mappedLicenceDetails.operationToTime = mappedLicenceDetails.updatableOperationToTime ? mappedLicenceDetails.operationToTime : null
+      licenceDetails.push(mappedLicenceDetails)
+    })
     return res.status(HttpStatus.OK).json(licenceDetails)
   } catch (e) {
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status)
+    handleError(res, e)
   }
 }
 
@@ -23,8 +30,7 @@ async function updateLicenceDetails(req, res) {
     const response = await patchOperationWithObjectId('ofm_licence_details', req.params.licenceDetailId, payload)
     return res.status(HttpStatus.OK).json(response)
   } catch (e) {
-    log.error(e)
-    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e.data ? e.data : e?.status)
+    handleError(res, e)
   }
 }
 
