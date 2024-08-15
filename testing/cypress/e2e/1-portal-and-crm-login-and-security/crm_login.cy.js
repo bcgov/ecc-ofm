d432f40056e37d1f1afef39b984b5af379092997
weskubo-cgi <Wesley.Kubo@gov.bc.ca>
2024-08-15 12:46:31 -0700

    Incorporated tests from qa-ofmcc-1250-crm-login.

--- testing/cypress/e2e/1-portal-and-crm-login-and-security/crm_login.cy.js
+++ testing/cypress/e2e/1-portal-and-crm-login-and-security/crm_login.cy.js
@@ -26,17 +26,52 @@ describe('Login to CRM via AAD', () => {
     })
   })
 
-  it.skip('failed login', () => {
+  it('navigate to organization', () => {
     cy.webExceptions(CRM_BASE_URL)
     cy.visit('https://google.ca')
-    cy.loginToAAD('doesnotexist@gov.bc.ca', 'password', CRM_URL, CRM_BASE_URL)
-
-    // Validate urror
-    cy.get('div[role="alert"]')
-      .contains(
-        'This username may be incorrect. Make sure you typed it correctly. Otherwise, contact your admin.'
-      )
-      .should('exist')
+    cy.loginToAAD(AAD_USERNAME, AAD_PASSWORD, CRM_URL, CRM_BASE_URL)
     cy.visit(CRM_URL)
+    cy.origin(CRM_BASE_URL, () => {
+      cy.on('uncaught:exception', (e) => {
+        return false
+      })
+
+      cy.get('ul[aria-label="Childcare Providers"]')
+        .contains('Organization-Facilities')
+        .click()
+      cy.get(
+        'input[aria-label="Organization-Facility Filter by keyword"]'
+      ).type('Test-1088 Organization{enter}')
+      cy.wait(5000)
+      cy.get('div[row-index="0"]')
+        .find('input[type="checkbox"]')
+        .click({ force: true })
+      cy.get('ul[aria-label="Organization-Facility Commands"]')
+        .find('button')
+        // XXX This should be Edit but for some reason it's not displaying
+        // Test is still valid as Org can be actioned with either button
+        .contains('Activate')
+        .click()
+    })
+  })
+
+  it('failed login', () => {
+    cy.webExceptions(CRM_BASE_URL)
+    cy.visit('https://google.ca')
+    //cy.loginToAAD('doesnotexist@gov.bc.ca', 'password', CRM_URL, CRM_BASE_URL)
+    cy.visit(CRM_BASE_URL)
+    cy.origin('login.microsoftonline.com', () => {
+      cy.get('input[type="email"]').type('doesnotexist@gov.bc.ca', {
+        log: false,
+      })
+      cy.get('input[type="submit"]').click()
+
+      // Validate error
+      cy.get('div[role="alert"]')
+        .contains(
+          'This username may be incorrect. Make sure you typed it correctly. Otherwise, contact your admin.'
+        )
+        .should('exist')
+    })
   })
 })
