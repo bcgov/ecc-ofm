b762c9e50b12eddce33a0406b619d4482ee8530b
weskubo-cgi <80127167+weskubo-cgi@users.noreply.github.com>
2024-08-21 16:12:23 -0700

    OFMCC-5687 - Messages UI updates (#320)
    * Updated default tab.
    Changed button styling on Messages.
    General button standardization.
    
    * Further button customization.
    
    * Replaced inline styles with classes.
    
    * ofmcc-5687 - adjust spacing between text and icon
    
    ---------
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>
    Co-authored-by: Le <vietdung.le@cgi.com>

--- frontend/src/components/funding/FundingAgreementsTab.vue
+++ frontend/src/components/funding/FundingAgreementsTab.vue
@@ -25,8 +25,8 @@
         </template>
         <template #[`item.actions`]="{ item }">
           <v-row no-gutters class="my-2 align-center justify-end justify-md-start">
-            <AppButton v-if="showSign(item)" :primary="false" size="small" height="30px" @click="goToPDFViewer(item)">Sign</AppButton>
-            <AppButton v-else-if="showOpen(item)" :primary="false" size="small" height="30px" @click="goToPDFViewer(item)">Open</AppButton>
+            <AppButton v-if="showSign(item)" :primary="false" size="small" @click="goToPDFViewer(item)">Sign</AppButton>
+            <AppButton v-else-if="showOpen(item)" :primary="false" size="small" @click="goToPDFViewer(item)">Open</AppButton>
           </v-row>
         </template>
       </v-data-table>
