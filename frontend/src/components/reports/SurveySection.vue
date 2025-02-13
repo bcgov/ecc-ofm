1f8c3b4f69b5ba4c962751dd9d8fcab9729a4130
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2025-02-13 10:25:38 -0800

    add a banner

--- frontend/src/components/reports/SurveySection.vue
+++ frontend/src/components/reports/SurveySection.vue
@@ -2,6 +2,11 @@
   <v-container>
     <div class="min-height-screen">
       <h2 class="pb-6">{{ section?.title }}</h2>
+
+      <AppAlertBanner v-if="section?.title === 'Human Resources'" type="info">
+        For your convenience, most of this information has been pre-filled based on information you provided in previous monthly reports. Please review carefully and update any changes as needed.
+      </AppAlertBanner>
+
       <v-form ref="form">
         <div v-for="question in questions" :key="question.questionId" class="mt-4 mb-8">
           <div v-if="isInstructions(question)" v-html="question?.additionalInfo" />
@@ -31,9 +36,10 @@ import AppLabel from '@/components/ui/AppLabel.vue'
 import reportMixin from '@/mixins/reportMixin'
 import SurveyQuestion from '@/components/reports/SurveyQuestion.vue'
 import SurveyTableQuestion from '@/components/reports/SurveyTableQuestion.vue'
+import AppAlertBanner from '@/components/ui/AppAlertBanner.vue'
 
 export default {
-  components: { AppLabel, SurveyQuestion, SurveyTableQuestion },
+  components: { AppLabel, SurveyQuestion, SurveyTableQuestion, AppAlertBanner },
   mixins: [reportMixin],
   props: {
     section: {
