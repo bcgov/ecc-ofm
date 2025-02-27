013373de49f0fb6a7b0ec4ca8f6038c4b0ecbede
Panika-cgi <panika.saxena@cgi.com>
2025-02-27 13:20:54 -0800

    OFMCC- 1119 updates to show active facilities only (#449)
    * Chips fixed to remove inactive facilities
    
    * Removed inactive facilities from "View"
    
    * Added facilityStateCode to flag inactive facilities.
    
    * Added conversion from object to id.
    
    * changes to show facility not in list to administer
    
    * clean up
    
    * clean up
    
    ---------
    
    Co-authored-by: weskubo-cgi <Wesley.Kubo@gov.bc.ca>

--- frontend/src/views/account-mgmt/ManageUsersView.vue
+++ frontend/src/views/account-mgmt/ManageUsersView.vue
@@ -59,7 +59,7 @@
                   <v-row>
                     <v-col cols="12" class="pt-0 pb-0">
                       <!-- Facilities table -->
-                      <v-data-table :headers="headersFacilities" :items="item.facilities" item-key="facilityId" items-per-page="-1" density="compact">
+                      <v-data-table :headers="headersFacilities" :items="getActiveFacilities(item.facilities)" item-key="facilityId" items-per-page="-1" density="compact">
                         <template v-slot:item.address="{ item }">{{ item.address }}, {{ item.city }}</template>
 
                         <template v-slot:item.isExpenseAuthority="{ item }">{{ item.isExpenseAuthority ? 'Yes' : 'No' }}</template>
@@ -151,6 +151,10 @@ export default {
     await this.getUsersAndFacilities()
   },
   methods: {
+    getActiveFacilities(facilities) {
+      return facilities.filter((facility) => facility.facilityStateCode == 0)
+    },
+
     isDeactivatedUser(user) {
       return user?.facilities?.length === 0
     },
