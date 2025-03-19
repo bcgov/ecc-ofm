0e539d5aeed9876f6c41def4d75fb04373d8abcf
Panika-cgi <panika.saxena@cgi.com>
2025-03-19 09:55:58 -0700

    fix for close bug

--- frontend/src/views/funding/BaseFundingView.vue
+++ frontend/src/views/funding/BaseFundingView.vue
@@ -94,7 +94,7 @@
       </v-col>
     </v-row>
   </v-container>
-  <AppDialog v-model="showDeclineDialog" title="Confirm" :isLoading="isLoading" persistent max-width="50%" @close="closeDialog">
+  <AppDialog v-model="showDeclineDialog" title="Confirm" :isLoading="isLoading" persistent max-width="50%" @close="showDeclineDialog = false">
     <template #content>
       <div class="confirm-dialog-text d-flex flex-column align-center">
         <strong>Are you sure you want to decline this funding agreement?</strong>
@@ -104,7 +104,7 @@
     <template #button>
       <v-row justify="space-around">
         <v-col cols="12" md="6" class="d-flex justify-center">
-          <AppButton id="dialog-go-back" :primary="false" size="large" width="250px" :loading="isLoading" @click="closeDialog">Go back</AppButton>
+          <AppButton id="dialog-go-back" :primary="false" size="large" width="250px" :loading="isLoading" @click="showDeclineDialog = false">Go back</AppButton>
         </v-col>
         <v-col cols="12" md="6" class="d-flex justify-center">
           <AppButton id="dialog-cancel-application" size="large" min-width="250px" max-width="450px" :loading="isLoading" @click="decline()">Decline Funding Agreement</AppButton>
