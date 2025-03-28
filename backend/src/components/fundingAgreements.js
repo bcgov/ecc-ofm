dd38e8dd63724efaa4efc10cab59001b013bbbd4
Jen Beckett <115109120+jenbeckett@users.noreply.github.com>
2025-03-28 09:25:01 -0700

    fix linter

--- backend/src/components/fundingAgreements.js
+++ backend/src/components/fundingAgreements.js
@@ -12,6 +12,7 @@ async function getFundingAgreements(req, res) {
   try {
     let fundingAgreements = []
     let operation = 'ofm_fundings?$select=ofm_fundingid,ofm_funding_number,ofm_declaration,ofm_start_date,ofm_end_date,_ofm_application_value,_ofm_facility_value,statuscode,statecode,ofm_version_number'
+
     if (req.query?.includeFundingEnvelopes) {
       operation +=
         ',ofm_envelope_hr_total,ofm_envelope_hr_wages_paidtimeoff,ofm_envelope_hr_benefits,ofm_envelope_hr_employerhealthtax,ofm_envelope_hr_prodevhours,ofm_envelope_hr_prodevexpenses,ofm_envelope_programming,ofm_envelope_administrative,ofm_envelope_operational,ofm_envelope_facility'
