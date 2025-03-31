fa254f27862d49d705eca25db3b44b8cd8d085e4
Panika-cgi <panika.saxena@cgi.com>
2025-03-31 11:15:19 -0700

    removed hiddencodes

--- frontend/src/views/applications/ApplicationsHistoryView.vue
+++ frontend/src/views/applications/ApplicationsHistoryView.vue
@@ -222,11 +222,10 @@ export default {
       return this.applications?.some((application) => ApplicationService.isValidApplication(application)) && this.hasGoodStanding
     },
     filteredApplicationItems() {
-      const hiddenCodes = [APPLICATION_STATUS_CODES.REDIRECTED]
       return this.sortApplicationItems(
         this.applicationItems
           .filter((application) => !this.facilityNameFilter || application.facilityName?.toLowerCase().includes(this.facilityNameFilter.toLowerCase()))
-          .filter((application) => !hiddenCodes.includes(application.statusCode)),
+          .filter((application) => application.statusCode !== APPLICATION_STATUS_CODES.REDIRECTED),
       )
     },
     ofmApplicationCardText() {
