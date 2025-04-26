de1f43da9d85e0c2441b6f0ca26747aa6d79e1b3
Panika-cgi <panika.saxena@cgi.com>
2025-04-25 22:35:09 -0700

    added consts to logic

--- frontend/src/stores/applications.js
+++ frontend/src/stores/applications.js
@@ -164,12 +164,22 @@ export const useApplicationsStore = defineStore('applications', {
       Operating Cost page
     */
     checkOperatingCostsComplete() {
+      const isRentLease = this.isRentLease(this.currentApplication)
+      const isMortgageOwned = this.isMortgageOwned(this.currentApplication)
+      const uploadedRentLeaseDocs = this.checkRequiredDocsExist(this.currentApplication, [DOCUMENT_TYPES.RENT_LEASE_AGREEMENT])
+      const uploadedRentLeaseAndFinancialDocs = this.checkRequiredDocsExist(this.currentApplication, [
+        DOCUMENT_TYPES.RENT_LEASE_AGREEMENT,
+        DOCUMENT_TYPES.INCOME_STATEMENT,
+        DOCUMENT_TYPES.BALANCE_SHEET,
+      ])
+      const uploadedMortgageDocs = this.checkRequiredDocsExist(this.currentApplication, [DOCUMENT_TYPES.MORTGAGE_STATEMENT])
+      const noFacilityTypeDocsRequired = !isRentLease && !isMortgageOwned
+
       const isFacilityTypeRequiredDocsUploaded =
-        (!this.isRentLease(this.currentApplication) && !this.isMortgageOwned(this.currentApplication)) ||
-        (this.isRentLease(this.currentApplication) &&
-          ((!this.isRenewal && this.checkRequiredDocsExist(this.currentApplication, [DOCUMENT_TYPES.RENT_LEASE_AGREEMENT, DOCUMENT_TYPES.INCOME_STATEMENT, DOCUMENT_TYPES.BALANCE_SHEET])) ||
-            (this.isRenewal && this.checkRequiredDocsExist(this.currentApplication, [DOCUMENT_TYPES.RENT_LEASE_AGREEMENT])))) ||
-        (this.isMortgageOwned(this.currentApplication) && this.checkRequiredDocsExist(this.currentApplication, [DOCUMENT_TYPES.MORTGAGE_STATEMENT]))
+        noFacilityTypeDocsRequired ||
+        (isRentLease && ((!this.isRenewal && uploadedRentLeaseAndFinancialDocs) || (this.isRenewal && uploadedRentLeaseDocs))) ||
+        (isMortgageOwned && uploadedMortgageDocs)
+
       const areCostsPositive = this.currentApplication?.totalYearlyOperatingCosts + this.currentApplication?.totalYearlyFacilityCosts > 0
       const isRentLeaseInformationComplete =
         !this.isRentLease(this.currentApplication) ||
